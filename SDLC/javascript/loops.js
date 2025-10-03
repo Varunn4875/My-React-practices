7950b1db63f5e1de73abf303fb0d71b25625508b
Varun Selvakumar <varunselvakumar6@gmail.com>
2025-10-03 12:09:45 +0530

    js

--- SDLC/javascript/loops.js
+++ SDLC/javascript/loops.js
@@ -1,3 +1,5 @@
+//6.nested-for-loop
+
 // for(let i=1; i<=5; i++){
 // 	console.log(`${i} th table`)
 //    for(let j=1; j<=10; j++){
@@ -9,6 +11,7 @@
 
 // 1. while loop
 let seconds=2
+
 while(seconds > 0){
 	// console.log("redirecting in seconds" + seconds)
 	seconds--;
@@ -26,30 +29,53 @@ while(number <=5)
 //3. do while with if 
 let attempts=0;
 let correct=false;
-let RealPassword='1234'
-// do{
-// 	 let enteredPasswd=prompt(`Enter your password Attempt No: ${attempts + 1} `)
-// 	attempts ++;
+let RealPassword=1234
+do{
+	 let enteredPasswd= parseInt(prompt(`Enter your password Attempt No: ${attempts + 1} `))
+	attempts ++;
 
-// 	if(enteredPasswd ===RealPassword){
-// 		console.log(`Access granted`)
+	if(enteredPasswd === RealPassword){
+		console.log(`Access granted`)
+		break;
+	}else{
+		console.log(`Access Denied   ${enteredPasswd}`)
+	}
+	if(attempts === 3){
+		console.log("Too many attempts. Exiting...")
+		break;
+	}
+}while(!correct)
+
+// do{
+// 	console.log("Enter password");
+// 	attempts++;
+
+// 	if(attempts===3){
+// 		console.log("Too many attempts. Exiting...");
 // 		break;
-// 	}else{
-// 		console.log(`Access Denied   ${enteredPasswd}`)
 // 	}
-// 	if(attempts === 3){
-// 		console.log("Too many attempts. Exiting...")
-// 		break;
-// 	}
-// }while(!correct)
+// }while(!correct);
+
 
 
 // 4. for-of-loop
-let fruits=["apple","banana","orange"]
+// let fruits=["apple","banana","orange"]
 
-for(let i=0; i<fruits.length; i++){
-	console.log(`${i}-${fruits[i]}`)
-}
-for(let fruit of fruits){
-	console.log(fruit)
-}
+// for(let i=0; i<fruits.length; i++){
+// 	console.log(`${i}-${fruits[i]}`)
+// }
+// for(let fruit of fruits){
+// 	console.log(fruit)
+// }
+
+//5. for-in-loop
+
+// const car = {
+//   make: "Toyota",
+//   model: "Corolla",
+//   year: 2020
+// };
+
+// for (let key in car) {
+//   console.log(`${key}: ${car[key]}`);
+// }
