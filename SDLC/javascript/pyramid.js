a5e72a5553234dc0fcea3a294de0040b222d2fb8
Varun Selvakumar <varunselvakumar6@gmail.com>
2025-10-07 13:22:30 +0530

    Update pyramid.js

--- SDLC/javascript/pyramid.js
+++ SDLC/javascript/pyramid.js
@@ -1,5 +1,34 @@
-let n=5;
+//1. Right Angled trialgle
 
-for(i=1; i<=n; i++){
-	console.log('#'.repeat(i))
-}  //right angled triangle
+let R=10;
+
+for(i=1; i<=R; i++){
+	// console.log('*'.repeat(i))
+}   //for inverted switch the values to (i=R;i>=1;i--)
+
+
+//2.Pyramid and Diamond  inverted
+
+// let n=5;
+// for (let i=1; i<=n; i++){
+// 	console.log(' '.repeat(n-i) + '#'.repeat(2*i-1))
+// }
+// for(let i=; i>=1;i--){
+// 	console.log(' '.repeat(n-i) + '#'.repeat(2*i-1))
+// }
+
+
+//3. number pattern
+let n=5
+
+for(i=1;i<=n;i++){
+	let line=''
+
+for(j=1; j<=i; j++){	
+	
+	 line += j+' ' 
+}
+console.log(line)
+}
+
+
