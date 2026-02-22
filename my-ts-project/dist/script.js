ef79390b0d39c08ce46313ada7b092061204e355
Varun Selvakumar <varunselvakumar6@gmail.com>
2026-02-22 21:13:56 +0530

    comm

--- my-ts-project/dist/script.js
+++ my-ts-project/dist/script.js
@@ -28,6 +28,12 @@ let pro1 = {
     name: "iphone",
 };
 console.log(pro1);
+//combining the above 
+const adminUser = {
+    role: "admin",
+    name: "varun"
+};
+console.log(adminUser);
 let id;
 id = '123';
 id: 123;
@@ -39,9 +45,65 @@ function updateStatus(status) {
     console.log(status);
 }
 updateStatus("pending");
-export {};
 //Type narrowing is the process by 
 //which TypeScript reduces a broader type 
 // (like a union) into a more specific type
 //  based on runtime checks.
+// let status = "success" | "error" | "loading"
+function printIDS(id) {
+    if (typeof id === "string") {
+        console.log(id.toUpperCase());
+    }
+    // else{
+    //     console.log(id.toFixed(2))
+    // }
+}
+printIDS(123);
+//enum role
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "admin";
+    Role["USER"] = "user";
+    Role["SUPER_ADMIN"] = "super_admin";
+})(Role || (Role = {}));
+let userRole = Role.ADMIN;
+console.log(userRole);
+//Generics
+//declaring typescript  as value:any   is equal to javascript 
+// function identify(value:any){
+//     return value;
+// }
+// console.log(identify("varun"))
+// <t>  <string>
+function identity(value) {
+    return value;
+}
+let result = identity("hello varun");
+console.log(result);
+const response = {
+    success: true,
+    data: "Data fetched successfully"
+};
+console.log(response);
+//generic constraints
+function getLength(item) {
+    return item.length;
+}
+console.log(getLength("helo"), getLength([1, 23, 3]));
+//multiple constraints
+//mergin two different objects
+function getsize(obj1, obj2) {
+    return Object.assign(Object.assign({}, obj1), obj2);
+}
+const size = getsize({ name: "varun" }, { age: 24 });
+console.log(size);
+const updatedUser = (data) => {
+    // only changed fields sent here 
+    return data.id;
+};
+function printpublicuser(user) {
+    console.log(user.id, user.name);
+}
+printpublicuser({ id: 1, name: "varun" });
+export {};
 //# sourceMappingURL=script.js.map
--- my-ts-project/script.ts
+++ my-ts-project/script.ts
@@ -70,6 +70,16 @@ type userDetails ={
 
 type Adminuser = admin & userDetails
 
+//combining the above 
+
+const adminUser:Adminuser={
+    role:"admin",
+    name:"varun"
+}
+console.log(adminUser)
+
+
+
 let id : string|number;
 
 id ='123';
@@ -91,4 +101,123 @@ updateStatus("pending")
 //which TypeScript reduces a broader type 
 // (like a union) into a more specific type
 //  based on runtime checks.
+
+
+// let status = "success" | "error" | "loading"
+
+function printIDS(id:string | number){
+    if (typeof id ==="string"){
+        console.log(id.toUpperCase())
+    }
+    // else{
+    //     console.log(id.toFixed(2))
+    // }
+}
+printIDS(123)
+
+
+//enum role
+
+enum Role{
+    ADMIN = "admin",
+    USER = "user",
+    SUPER_ADMIN = "super_admin"
+}
+let userRole: Role = Role.ADMIN
+console.log(userRole)
+
+
+//Generics
+//declaring typescript  as value:any   is equal to javascript 
+
+// function identify(value:any){
+//     return value;
+// }
+
+// console.log(identify("varun"))
+
+// <t>  <string>
+
+function identity <T>(value:T):T{
+    return value
+}
+
+let result = identity <string> ("hello varun")
+
+console.log(result)
+
+interface ApiResponce <t> {
+    success:boolean;
+    data:t
+}
+const response: ApiResponce <string> ={
+    success:true,
+    data:"Data fetched successfully"
+}
+console.log(response);
+
+//generic constraints
+
+function getLength <t extends {length:number}>(item:t):number{
+   return item.length
+}
+console.log(getLength("helo"),
+getLength([1,23,3]))
+
+//multiple constraints
+
+//mergin two different objects
+
+function getsize <t,u>(obj1:t,obj2:u): t&u{
+return{...obj1, ...obj2}
+}
+const size = getsize({name:"varun"},{age:24})
+console.log(size);
+
+
+//utility generic types
+
+//  1.  partial<T>
+
+//patch method
+//redux reducers
+//redux state updates
+
+type partialUser ={
+    id:number,
+    name:string,
+}
+type updatedUser =Partial<partialUser>
+
+const updatedUser= (data:Partial<partialUser>) =>{
+    // only changed fields sent here 
+       return data.id
+
+}
+// updatedUser(1)
+
+// 2. Required<t>
+// 3. Readonly<t>   cannot update the value
+// 4. pick<t,k>
+
+//select only some keys
+
+type pickUser ={
+     id:number,
+    name:string,
+    email:string
+}
+
+type publicuser= Pick<pickUser , "id" | "name">
+
+function printpublicuser(user:publicuser): void{
+    console.log(user.id,user.name)
+}
+printpublicuser({id:1,name:"varun"})
+
+
+
+
+
+
 
